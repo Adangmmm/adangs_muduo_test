069bf567041201f07a01e096c3b5fd4fdd9cea42
adangmmm <adangmmm@163.com>
2025-03-13 17:09:57 +0800

    250313 第二次提交

---
+++ include/ads_InetAddress.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <arpa/inet.h>  //包含了 IP 地址转换相关的函数（如 inet_pton、inet_ntop 等
+#include <netinet/in.h> //定义了与 sockaddr_in 结构体和网络地址相关的常量和数据结构（如 htons、ntohs 等）。这个头文件是 Linux/Unix 网络编程的基础
+#include <string>
+
+// 这个InetAddress类是muduo库中用于封装 IPv4 套接字地址（socket address） 的一个工具类，方便在网络编程中管理和操作地址信息
+// InetAddress类的主要功能是将 IP 地址和端口号封装成一个对象，方便在网络编程中传递和使用
+// InetAddress 简化了对 sockaddr_in 的操作，避免直接操作底层的 C 语言接口。
+class InetAddress{
+public:
+    // explicit 关键字防止构造函数被隐式转换调用，防止发生隐式转换带来的歧义问题。
+    // uint16_t port = 0：默认端口号为 0（表示由操作系统自动分配端口）。std::string ip = "127.0.0.1"：默认 IP 地址为 127.0.0.1（本地主机）
+    explicit InetAddress(unit16_t port = 0, std::string ip = "127.0.0.1");
+    // 允许通过一个 sockaddr_in 结构体直接初始化 InetAddress 对象。使用 成员初始化列表 直接对 addr_ 进行赋值。
+    explicit InetAddress(const sockaddr_in &addr)
+        : addr_(addr)
+        {
+        }
+    
+    // 获取 IP 地址。 将 sockaddr_in 结构体中的 IP 地址转换为字符串格式（如 "192.168.1.1"）。const 关键字表示这个成员函数是只读的，不会修改类的成员变量。
+    std::string toIp() const;
+    // 获取 IP 地址和 端口号。将 sockaddr_in 结构体中的 IP 和端口号转换为 "192.168.1.1:8080" 格式。
+    std::string toIpPort() const;
+    // 返回 sockaddr_in 结构体中的端口号（通过 ntohs() 将网络字节序转换为主机字节序）。
+    unit16_t toPort() const; 
+
+    // 返回 sockaddr_in 结构体的指针。const 表示调用这个函数不会修改对象的状态。
+    /* 两个const:
+     * 1.返回类型中的 const：表示返回的指针指向的数据（即 sockaddr_in 结构体）是 常量的，调用者不能通过返回的指针来修改数据。
+     * 2.函数声明末尾的 const：表示这个函数是一个 常量成员函数，即在函数内部不会修改对象的状态（不会修改任何成员变量）。
+     */
+    const sockaddr_in *getSockAddr() const {return &addr_;}
+
+    // 通过一个 sockaddr_in 结构体来修改 InetAddress 对象的地址信息。
+    void setSockAddr(const sockaddr_in &addr) {addr_ = addr;}
+
+private:
+    // sockaddr_in 是一个用于存储 IPv4 地址 的结构体
+    // 定义在 <netinet/in.h> 中。addr_ 用于保存 IP 地址和端口号。
+    sockaddr_in addr_;  
+}
---
+++ src/ads_InetAddress.cc
@@ -0,0 +1,60 @@
+#include <strings.h>
+#include <string.h>
+
+#include "ads_InetAddress.h"
+
+InetAddress::InetAddress(unit16_t port, std:string ip){
+    // memset(void *s, int c, size_t n) —— 用于将内存块设置为指定的值。
+    // &addr_ —— 指向要清空的内存（即 sockaddr_in 结构体）。0 —— 用 0 填充内存。sizeof(addr_) —— 填充的字节长度。
+    // 目的是将 sockaddr_in 结构体的内存置为 0，防止其中存在未定义数据（初始化操作）
+    ::memset(&addr_, 0, sizeof(addr_));
+    // 设置地址族为 IPv4
+    addr_.sin_family = AF_INET;
+    // htons(uint16_t hostshort) —— 将 主机字节序（主机端存储方式）转换为 网络字节序（大端字节序）。在不同架构主机之间保证兼容性
+    addr_.sin_port = htons(port);
+    // ::inet_addr(const char *cp) —— 将点分十进制的 IP 地址（如 "192.168.1.1") 转换为 in_addr_t 类型的二进制格式（网络字节序）。
+    // ip.c_str() —— 将 std::string 转换为 C 字符串（即 const char *）。
+    addr_.sin_addr.s_addr = inet_addr(ip.c_str());
+
+    // 推荐使用以下
+    /*
+     * if (::inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr) <= 0) {
+        // 处理错误，例如抛出异常或记录日志
+     * }
+     */
+}
+
+std::string InetAddress::toIp() const{
+    // 定义一个长度为 64 的缓冲区，初始化列表为0，保证内容干净
+    char buf[64] = {0};
+    // inet_ntop（network to presentation）将网络格式（即二进制）转换为人类可读的点分十进制格式。
+    //inet_ntop() 的返回值是指向 buf 的指针（即转换后的 IP 字符串）。如果转换失败，返回 nullptr。把网络字符
+    ::inet_ntop(AF_INET, &addr.sin_addr, buf, sizeof(buf));
+    return buf;
+}
+
+std::string InetAddress::toIpPort() const{
+    char buf[64] = {0};
+    ::inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof(buf));
+    // ::strlen(buf) 是一个 C 标准库函数，用于返回字符串 buf 的长度（不包括末尾的 '\0'）。end 保存当前字符串的长度。
+    size_t end = strlen(buf);
+    // ntohs(uint16_t netshort) —— 将 网络字节序（大端字节序）转换为 主机字节序（主机端存储方式）。
+    unit16_t port = ntohs(addr_.sin_port);
+    // sprintf() 的工作方式是：从 buf + end 的位置开始写入格式化字符串，在写入的最后自动加上 '\0' 结束符
+    sprintf(buf + end, ":%u", port);
+    return buf;
+}
+
+unit16_t InetAddress::toPort() const{
+    return ntohs(addr_.sin_port);
+}
+
+
+// 测试代码块
+#if 0
+#include <iostream>
+int main(){
+    InetAddress addr(8080);
+    std::cout << addr.toIpPort() << std:endl;
+}
+#endif
